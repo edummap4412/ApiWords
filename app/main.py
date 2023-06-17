6479a941268c25dd0b6d4b08159326e32a11da9a
edummap4412 <mascarenhaseduardo4412@gmail.com>
2023-06-17 02:07:25 -0300

    feat:apiwords

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from config.sql_alchemy import models
+from config.sql_alchemy.config_sql_alchemy import engine
+from app.router import router
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+@app.get('/')
+async def Home():
+    return "Welcome Home"
+
+
+app.include_router(router, prefix='/api', tags=['api'])
---
+++ config/sql_alchemy/schemas.py
@@ -0,0 +1,20 @@
+from typing import Optional, Generic, TypeVar
+from pydantic import BaseModel, Field
+from pydantic.generics import GenericModel
+
+T = TypeVar("T")
+
+
+class Response(GenericModel, Generic[T]):
+    code: str
+    status: str
+    message: str
+    result: Optional[T]
+    
+
+class WordSchema(BaseModel):
+    word: Optional[str] = None
+
+
+class RequestWord(BaseModel):
+    parameter: WordSchema = Field(...)
